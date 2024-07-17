08780e1e57ab9ffd380bf0b8769de5ee588b4b8c
Manish Maharjan <maharjanm96@gmail.com>
2024-07-17 06:57:12 +0545

    Session Wrapper

--- components/UserDashboard.tsx
+++ components/UserDashboard.tsx
@@ -1,19 +1,31 @@
-import Button from "./Button";
+"use client";
 
-export default function UserInfo() {
+import { useSession } from "next-auth/react";
+
+const UserDashboard = () => {
+  const { data: session } = useSession();
+
   return (
-    <div className="grid place-items-center h-screen">
-      <div className="flex flex-col gap-2 items-center shaodw-lg bg-slate-100 rounded-lg p-4 my-6 ">
-        <div>
-          Name: <span className="font-bold">Manish Maharjan</span>
+    <>
+      {session ? (
+        <div className="grid place-items-center h-screen">
+          <div className="flex flex-col gap-2 items-center shaodw-lg bg-slate-100 rounded-lg p-4 my-6 ">
+            <div>
+              Name: <span className="font-bold">Manish Maharjan</span>
+            </div>
+            <div>
+              Email: <span className="font-bold">maharjanm96@gmail.com</span>
+            </div>
+            <div className="flex justify-center"></div>
+          </div>
         </div>
-        <div>
-          Email: <span className="font-bold">maharjanm96@gmail.com</span>
-        </div>
-        <div className="flex justify-center">
-          <Button name="Logout" href="" />
-        </div>
-      </div>
-    </div>
+      ) : (
+        <>
+          <center>You are not logged in.</center>
+        </>
+      )}
+    </>
   );
-}
+};
+
+export default UserDashboard;
